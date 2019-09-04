26784b3aff5653e959e0f139e5cb707728f13612
Angel Vargas <angelvargaspool@gmail.con>
2019-09-04 17:40:12 -0500

    string exercises

---
+++ strings.py
@@ -0,0 +1,9 @@
+text = "I am a coder"
+_replace = text.replace(" ","-")
+print('Reemplaza :: '+_replace)
+_index = text.index('am')
+print(_index)
+_verify = text.endswith("er")
+print(_verify)
+_count = text.count("a")
+print(_count)
